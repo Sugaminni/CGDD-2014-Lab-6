ccbe8f18fd4b56d9b7bff2b3e04e3e7038d8d2a6
Alex Lu <Alexhonglu@gmail.com>
2025-10-17 23:57:42 -0400

    Added Pieces Enum

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -13,6 +13,12 @@ public class BoardManager : MonoBehaviour
         board = new Pieces[8, 8]; // Initialize the board
     }
 
+     private enum Pieces
+    {
+        None = -1,
+        BLACK_KING, BLACK_QUEEN, BLACK_BISHOP, BLACK_KNIGHT, BLACK_ROOK, BLACK_PAWN,
+        WHITE_KING, WHITE_QUEEN, WHITE_BISHOP, WHITE_KNIGHT, WHITE_ROOK, WHITE_PAWN
+    }
 
     // Update is called once per frame
     void Update()
